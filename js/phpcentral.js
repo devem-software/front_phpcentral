5dba683edadca7ee9488875ee888e1d08bc09a65
EdwinMarroquin <60857232+devem-software@users.noreply.github.com>
2023-11-03 19:10:43 -0500

    Commit 001
    Changes to be committed:
    	new file:   .prettierrc
    	new file:   .vscode/settings.json
    	new file:   Index.html
    	new file:   css/app.css
    	new file:   css/general.css
    	new file:   css/logos.css
    	new file:   js/phpcentral.js

---
+++ js/phpcentral.js
@@ -0,0 +1,9 @@
+document
+    .getElementById("menu_wrapper")
+    .addEventListener("click", (e) => {
+        document.getElementById("menu_container").classList.toggle("show_menu")
+        let icon = document.getElementById("menu_wrapper").childNodes[1].childNodes[1]
+        icon.classList.toggle('mdi-menu')
+        icon.classList.toggle('mdi-close')
+    }
+    )
