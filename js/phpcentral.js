f70229dfc181b876cf86944de041c4e4aceb82d2
Edwin Marroquin devem <60857232+devem-software@users.noreply.github.com>
2023-11-19 14:47:27 -0500

    update js/phpcentral.js

--- js/phpcentral.js
+++ js/phpcentral.js
@@ -14,14 +14,14 @@ const store = {
     if (this.get(key) !== undefined) this.set(key, value)
   }
 }
-
-$("#menu_wrapper").addEventListener("click", () => {
-    $("#menu_container").classList.toggle("show_menu")
-    let icon = $("#menu_wrapper").childNodes[1]
-    icon.classList.toggle('mdi-menu')
-    icon.classList.toggle('mdi-close')
-  }
-  )
+// 
+// $("#menu_wrapper").addEventListener("click", () => {
+//     $("#menu_container").classList.toggle("show_menu")
+//     let icon = $("#menu_wrapper").childNodes[1]
+//     icon.classList.toggle('mdi-menu')
+//     icon.classList.toggle('mdi-close')
+//   }
+//   )
 
 $("#switch_theme").addEventListener("click", () => {
   document.body.classList.toggle("dark")
