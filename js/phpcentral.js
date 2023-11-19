885d9a36793c34c204c76afc22f929c9cc0a0801
EdwinMarroquin <60857232+devem-software@users.noreply.github.com>
2023-11-19 12:59:07 -0500

    Refactor styles
    Add new scss styles

--- js/phpcentral.js
+++ js/phpcentral.js
@@ -1,13 +1,27 @@
-document
-    .getElementById("menu_wrapper")
+const $ = (_el) => document.querySelector(_el)
+const $all = (_el) => document.querySelectorAll(_el)
+
+const store = () => {
+    function get (key) {
+        return localStorage.getItem('key')
+    }
+    function set (key, value) {
+        localStorage.setItem(key, value)
+    }
+    function findReplace (key, value) {
+        if (this.get(key) !== undefined) this.set(key, value)
+    }
+}
+
+$("#menu_wrapper")
     .addEventListener("click", (e) => {
-        document.getElementById("menu_container").classList.toggle("show_menu")
-        let icon = document.getElementById("menu_wrapper").childNodes[1].childNodes[1]
+        $("#menu_container").classList.toggle("show_menu")
+        let icon = $("#menu_wrapper").childNodes[1].childNodes[1]
         icon.classList.toggle('mdi-menu')
         icon.classList.toggle('mdi-close')
     }
     )
 
-document.getElementById("switch_theme").addEventListener("click", () => {
+$("#switch_theme").addEventListener("click", () => {
     document.body.classList.toggle("dark")
 })
