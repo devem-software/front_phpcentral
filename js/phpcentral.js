607541c46aa2719221a3a451e02bf695d115c1ef
Edwin Marroquin devem <60857232+devem-software@users.noreply.github.com>
2023-11-19 14:36:19 -0500

    update js/phpcentral.js

--- js/phpcentral.js
+++ js/phpcentral.js
@@ -2,26 +2,26 @@ const $ = (_el) => document.querySelector(_el)
 const $all = (_el) => document.querySelectorAll(_el)
 
 const store = {
-    get: (key)=> {
-        return localStorage.getItem('key')
-    }
-    set: (key, value) =>{
-        localStorage.setItem(key, value)
-    }
-    findReplace: (key, value) =>{
-        if (this.get(key) !== undefined) this.set(key, value)
-    }
+  get: (key) => {
+    return localStorage.getItem('key')
+  },
+    set: (key, value) => {
+    localStorage.setItem(key, value)
+  },
+    findReplace: (key, value) => {
+    if (this.get(key) !== undefined) this.set(key, value)
+  }
 }
 
 $("#menu_wrapper")
-    .addEventListener("click", (e) => {
-        $("#menu_container").classList.toggle("show_menu")
-        let icon = $("#menu_wrapper").childNodes[1].childNodes[1]
-        icon.classList.toggle('mdi-menu')
-        icon.classList.toggle('mdi-close')
-    }
-    )
+  .addEventListener("click", (e) => {
+    $("#menu_container").classList.toggle("show_menu")
+    let icon = $("#menu_wrapper").childNodes[1].childNodes[1]
+    icon.classList.toggle('mdi-menu')
+    icon.classList.toggle('mdi-close')
+  }
+  )
 
 $("#switch_theme").addEventListener("click", () => {
-    document.body.classList.toggle("dark")
+  document.body.classList.toggle("dark")
 })
