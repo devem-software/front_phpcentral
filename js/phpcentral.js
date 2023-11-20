0a43a763853bc6bd72dd0a7cd2ef57efc7585261
EdwinMarroquin <60857232+devem-software@users.noreply.github.com>
2023-11-20 06:40:17 -0500

    update script
    implement primitive localstorage

--- js/phpcentral.js
+++ js/phpcentral.js
@@ -1,7 +1,7 @@
 var $ = (_el) => document.querySelector(_el);
 var $all = (_el) => document.querySelectorAll(_el);
 
-console.log($("#menu_wrapper"));
+console.log($('#menu_button'));
 
 const store = {
     get: (key) => {
@@ -10,19 +10,24 @@ const store = {
     set: (key, value) => {
         localStorage.setItem(key, value)
     },
-    findReplace: (key, value) => {
+    update: (key, value) => {
         if (this.get(key) !== undefined) this.set(key, value)
-    }
+    },
+    validate: (key) => (this.get(key) === undefined) ? false : true,
 }
 
-$("#menu_wrapper").addEventListener("click", () => {
-    $("#menu_container").classList.toggle("show_menu")
-    let icon = $("#menu_wrapper").childNodes[1]
+$('#menu_button').addEventListener('click', () => {
+    $('#menu_container').classList.toggle('show_menu')
+    let icon = $('#menu_button').childNodes[1]
     icon.classList.toggle('mdi-menu')
     icon.classList.toggle('mdi-close')
 }
 )
 
-$("#switch_theme").addEventListener("click", () => {
-    document.body.classList.toggle("dark")
+$('#switch_theme').addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
+
+window.addEventListener('load', () => {
+    document.body.classList.toggle('dark');
 })
